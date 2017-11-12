ee21a579e062d5de461506d8954f98a1aac23f4f
rburbidge <ryan.burbidge@gmail.com>
2017-11-11 16:44:38 -0800

    Remove unused import

--- Squid/app/src/main/java/com/sirnommington/squid/activity/prefs/PreferencesFragment.java
+++ Squid/app/src/main/java/com/sirnommington/squid/activity/prefs/PreferencesFragment.java
@@ -1,7 +1,6 @@
 package com.sirnommington.squid.activity.prefs;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
