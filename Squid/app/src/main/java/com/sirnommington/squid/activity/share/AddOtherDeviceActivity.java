65b36a366dbf6bcbd37c2005d0cf125222754610
rburbidge <ryan.burbidge@gmail.com>
2017-12-04 09:36:14 -0800

    - Initial version of AddAnotherDeviceActivity - Updated AboutActivity to have ActionBar. This is done by having it subclass AppCompatActivity.

--- Squid/app/src/main/java/com/sirnommington/squid/activity/share/AddOtherDeviceActivity.java
+++ Squid/app/src/main/java/com/sirnommington/squid/activity/share/AddOtherDeviceActivity.java
@@ -1,18 +1,17 @@
-package com.sirnommington.squid.activity.prefs;
+package com.sirnommington.squid.activity.share;
 
 import android.os.Bundle;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 
 import com.sirnommington.squid.R;
 
-/**
- * About screen for the app. Shows credits, etc.
- */
-public class AboutActivity extends AppCompatActivity {
+public class AddOtherDeviceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_add_other_device);
     }
+
 }
