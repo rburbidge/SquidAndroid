5be5cd92a539b0e8f230840eb807e4c0c1275d84
rburbidge <ryan.burbidge@gmail.com>
2017-12-03 22:27:34 -0800

    Moved implementation of ShareLinkEntryActivity activity launching onto background task. (#12)
    This is because using Google silentSignIn() was throwing an IllegalStateException due to being launched on the UI thread.

--- Squid/app/src/main/java/com/sirnommington/squid/activity/share/ShareLinkEntryActivity.java
+++ Squid/app/src/main/java/com/sirnommington/squid/activity/share/ShareLinkEntryActivity.java
@@ -1,6 +1,7 @@
 package com.sirnommington.squid.activity.share;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -19,23 +20,38 @@ public class ShareLinkEntryActivity extends AppCompatActivity {
     private static final String TAG = ShareLinkEntryActivity.class.getSimpleName();
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                launchCorrectActivity();
+                return null;
+            }
+        }.execute();
+    }
+
+    /**
+     * Launches the correct activity based on the state of the app.
+     *
+     * This is launched on a background thread due to use of silentSignIn().
+     */
+    private void launchCorrectActivity() {
         final Preferences preferences = new Preferences(this);
 
         // Determine if we need to launch the IntroActivity for any reason
         Integer introStep = null;
-        if(!preferences.isInitialized()) {
+        if (!preferences.isInitialized()) {
             introStep = new Integer(IntroStep.DESCRIPTION);
         } else {
             final boolean isSignedIn = new GoogleSignIn(this).silentSignIn() != null;
-            if(!isSignedIn) {
+            if (!isSignedIn) {
                 introStep = new Integer(IntroStep.SIGN_IN);
             }
         }
 
-        if(introStep != null) {
+        if (introStep != null) {
             // If we need to launch the intro flow, then do that
             // TODO: At the moment this does not send the URL once the intro flow is complete, however we want to enable
             // that int the future.
