551df862dd07aa80db6d777bcff76f0af4ee11d2
rburbidge <ryan.burbidge@gmail.com>
2017-12-05 22:34:48 -0800

    Add ActionBar to PreferencesActivity (#14)
    Add ActionBar to PreferencesActivity by having it subclass AppCompatActivity

--- Squid/app/src/main/java/com/sirnommington/squid/activity/prefs/PreferencesActivity.java
+++ Squid/app/src/main/java/com/sirnommington/squid/activity/prefs/PreferencesActivity.java
@@ -2,13 +2,14 @@ package com.sirnommington.squid.activity.prefs;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 
 import com.sirnommington.squid.R;
 
 /**
  * Shows app settings.
  */
-public class PreferencesActivity extends Activity {
+public class PreferencesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
