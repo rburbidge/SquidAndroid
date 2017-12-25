c354bc8d5f983f321d626d52dec5b4536d8bfa80
rburbidge <ryan.burbidge@gmail.com>
2017-12-24 22:00:23 -0800

    Show error when retrieving devices fails - Updated SquidService.getDevices() to return AsyncResponse - Updated AddDeviceFragment AsyncTask to return AsyncResponse

--- Squid/app/src/main/java/com/sirnommington/squid/activity/common/AsyncResponse.java
+++ Squid/app/src/main/java/com/sirnommington/squid/activity/common/AsyncResponse.java
@@ -8,7 +8,15 @@ public class AsyncResponse<TPayload> {
     public final TPayload payload;
     public final Object error;
 
-    public AsyncResponse(TPayload payload, Object error) {
+    public static <TPayload> AsyncResponse<TPayload> create(TPayload payload) {
+        return new AsyncResponse(payload, null);
+    }
+
+    public static <TPayload> AsyncResponse<TPayload> createError(Object error) {
+        return new AsyncResponse(null, error);
+    }
+
+    private AsyncResponse(TPayload payload, Object error) {
         this.payload = payload;
         this.error = error;
     }
--- Squid/app/src/main/java/com/sirnommington/squid/services/squid/SquidService.java
+++ Squid/app/src/main/java/com/sirnommington/squid/services/squid/SquidService.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.sirnommington.squid.activity.common.AsyncResponse;
 import com.sirnommington.squid.services.google.GoogleSignIn;
 
 import org.json.JSONException;
@@ -16,6 +17,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.Collection;
 
@@ -24,7 +27,8 @@ import java.util.Collection;
  */
 public class SquidService {
 
-    private static final String TAG = SquidService.class.getSimpleName();;
+    private static final String TAG = SquidService.class.getSimpleName();
+    private static final int TIMEOUT_MILLIS = 30000;
 
     private final String endpoint;
     private final GoogleSignIn googleSignIn;
@@ -43,10 +47,9 @@ public class SquidService {
      * @param name The device name.
      * @param gcmToken The device GCM token.
      * @return Result indicating added, already existed, etc.
-     * @throws IOException If there is an issue sending the request.
      * @throws JSONException If there is an issue constructing the outgoing request.
      */
-    public AddDeviceResult addDevice(String name, String gcmToken) throws IOException, JSONException {
+    public AddDeviceResult addDevice(String name, String gcmToken) throws JSONException {
         final JSONObject body = new JSONObject();
         body.put("name", name);
         body.put("gcmToken", gcmToken);
@@ -58,7 +61,8 @@ public class SquidService {
             }
         });
         if(response.statusCode != 200 && response.statusCode != 302) {
-            throw new IOException("An error occurred while adding the device");
+            // TODO Figure out how to return errors
+            //throw new IOException("An error occurred while adding the device");
         }
 
         return new AddDeviceResult(response.body, response.statusCode == 200);
@@ -68,23 +72,18 @@ public class SquidService {
      * Removes a device.
      * @param deviceId The
      * @return True if the device was successfully removed.
-     * @throws IOException If there is an issue sending the request.
-     * @throws JSONException If there is an issue parsing the response.
      */
-    public boolean removeDevice(String deviceId) throws IOException, JSONException {
+    public boolean removeDevice(String deviceId) {
         final HttpResponse response = this.sendRequest("DELETE", "/api/devices/" + deviceId, null, null);
         return response.statusCode == 200;
     }
 
     /**
      * Gets the user's devices.
-     *
      * TODO What does this return if the user doesn't exist or had no devices?
-     * @return The devices, or null if an error occurred.
-     * @throws IOException If there is an issue sending the request.
-     * @throws JSONException If there is an issue parsing the response.
+     * @return The devices, or an error.
      */
-    public Collection<DeviceModel> getDevices() throws IOException, JSONException {
+    public AsyncResponse<Collection<DeviceModel>> getDevices() {
         final HttpResponse<Collection<DeviceModel>> response = this.sendRequest("GET", "/api/devices", null,
             new JsonParser() {
                 @Override
@@ -94,21 +93,21 @@ public class SquidService {
                     return gson.fromJson(jsonString, type);
                 }
             });
+
         if(response.statusCode == 200) {
-            return response.body;
+            return AsyncResponse.create(response.body);
         }
 
-        return null;
+        return AsyncResponse.createError(response.statusCode);
     }
 
     /**
      * Sends the URL to a device.
      * @param deviceId The recipient device ID.
      * @param url The URL to send.
-     * @throws IOException If there is an issue sending the request.
      * @throws JSONException If there is an issue parsing the response.
      */
-    public void sendUrl(String deviceId, String url) throws JSONException, IOException {
+    public void sendUrl(String deviceId, String url) throws JSONException {
         final JSONObject body = new JSONObject();
         body.put("url", url);
 
@@ -134,14 +133,13 @@ public class SquidService {
      * @param requestMethod GET, PUT, etc.
      * @param relativePath The relative path. Must be preceded with a forward slash.
      * @param requestBody The JSON request body, if any.
-     * @throws IOException If there is an issue sending the request.
      * @return The HTTP response.
      */
     private HttpResponse sendRequest(
             final String requestMethod,
             final String relativePath,
             final JSONObject requestBody,
-            final JsonParser responseParser) throws IOException, JSONException {
+            final JsonParser responseParser) {
         // Bomb out early if we can't get an ID token for authentication
         final String idToken = this.googleSignIn.silentSignIn();
         if(idToken == null) {
@@ -150,37 +148,58 @@ public class SquidService {
             return new HttpResponse(401, errorMessage);
         }
 
-        URL url = new URL(this.endpoint + relativePath);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod(requestMethod);
-
-        // Header prefixes are one of the following:
-        // 'Bearer Google OAuth Access Token='
-        // 'Bearer Google OAuth ID Token='
-        // See http://stackoverflow.com/questions/8311836/how-to-identify-a-google-oauth2-user/13016081#13016081 for
-        // details on Google access vs. ID tokens
-        conn.setRequestProperty("Authorization", "Bearer Google OAuth ID Token=" + idToken);
-
-        if(requestBody != null) {
-            conn.setRequestProperty("Content-Type", "application/json");
+        // Create the URL
+        final String urlString = this.endpoint + relativePath;
+        URL url;
+        try {
+            url = new URL(urlString);
+        } catch(MalformedURLException e) {
+            Log.e(TAG, "Invalid URL=" + urlString + ", e=" + e);
+            return new HttpResponse(500, null);
         }
 
-        conn.connect();
+        try {
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            try {
+                conn.setRequestMethod(requestMethod);
+            } catch (ProtocolException e) {
+                Log.e(TAG, "Invalid HTTP method=" + requestMethod + ", e=" + e);
+                return new HttpResponse(500, null);
+            }
+            conn.setConnectTimeout(TIMEOUT_MILLIS);
 
-        if(requestBody != null) {
-            DataOutputStream output = new DataOutputStream(conn.getOutputStream());
-            output.writeBytes(requestBody.toString());
-            output.flush ();
-            output.close ();
-        }
+            // Header prefixes are one of the following:
+            // 'Bearer Google OAuth Access Token='
+            // 'Bearer Google OAuth ID Token='
+            // See http://stackoverflow.com/questions/8311836/how-to-identify-a-google-oauth2-user/13016081#13016081 for
+            // details on Google access vs. ID tokens
+            conn.setRequestProperty("Authorization", "Bearer Google OAuth ID Token=" + idToken);
 
-        final int responseCode = conn.getResponseCode();
-        Object parsedBody = null;
-        if(responseParser != null && (responseCode == 200 || responseCode == 302)) {
-            final String bodyString = readAll(conn.getInputStream());
-            parsedBody = responseParser.parse(bodyString);
+            if (requestBody != null) {
+                conn.setRequestProperty("Content-Type", "application/json");
+            }
+
+            conn.connect();
+
+            if (requestBody != null) {
+                DataOutputStream output = new DataOutputStream(conn.getOutputStream());
+                output.writeBytes(requestBody.toString());
+                output.flush();
+                output.close();
+            }
+
+            final int responseCode = conn.getResponseCode();
+            Object parsedBody = null;
+            if (responseParser != null && (responseCode == 200 || responseCode == 302)) {
+                final String bodyString = readAll(conn.getInputStream());
+                parsedBody = responseParser.parse(bodyString);
+            }
+            return new HttpResponse<>(responseCode, parsedBody);
+        } catch(IOException e) {
+            return new HttpResponse(500, null);
+        } catch(JSONException e) {
+            return new HttpResponse(500, null);
         }
-        return new HttpResponse<>(responseCode, parsedBody);
     }
 
     /**
