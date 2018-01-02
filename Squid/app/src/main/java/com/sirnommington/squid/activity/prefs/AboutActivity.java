02402c176a5bde01f248ccd95b72cad28772655b
rburbidge <ryan.burbidge@gmail.com>
2018-01-01 21:26:59 -0800

    When user touches About page 5 times, dev options is enabled

--- Squid/app/src/main/java/com/sirnommington/squid/activity/prefs/AboutActivity.java
+++ Squid/app/src/main/java/com/sirnommington/squid/activity/prefs/AboutActivity.java
@@ -2,17 +2,41 @@ package com.sirnommington.squid.activity.prefs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
 import com.sirnommington.squid.R;
+import com.sirnommington.squid.services.Preferences;
 
 /**
  * About screen for the app. Shows credits, etc.
  */
 public class AboutActivity extends AppCompatActivity {
 
+    private final AboutActivity thiz = this;
+    private final int ENABLE_DEV_OPTIONS_TOUCH_COUNT = 5;
+    private int touchCount = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        this.setContentView(R.layout.activity_about);
+
+        final Preferences preferences = new Preferences(this);
+
+        final View layout = this.findViewById(R.id.about);
+        layout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                touchCount++;
+                if(touchCount == ENABLE_DEV_OPTIONS_TOUCH_COUNT) {
+                    preferences.enableDevMode();
+                    Toast.makeText(thiz, R.string.preferences_about_dev_mode, Toast.LENGTH_LONG).show();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
